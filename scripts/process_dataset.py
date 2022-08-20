20d372c5604ca1397b6be275f59d248b2e2aa493
mreichhoff <matthew.reichhoff@gmail.com>
2022-08-19 20:17:09 -0400

    Add indexing on base words
    i.e., for a base language word, find the 10 simplest target
    language sentences whose parallel includes that word

--- scripts/process_dataset.py
+++ scripts/process_dataset.py
@@ -72,28 +72,45 @@ def main():
     parser = argparse.ArgumentParser(
         description='Get the simplest possible examples for each word in a dataset')
     parser.add_argument(
-        '--language', help='a lowercase language name, like chinese or english.')
+        '--base-language', help='a lowercase language name, like chinese or english.')
+    parser.add_argument(
+        '--target-language', help='a lowercase language name, like chinese or english.')
     parser.add_argument(
         '--base-sentences-filename', help='the filename of a list of sentences in the base language')
     parser.add_argument(
         '--target-sentences-filename', help='the filename of a list of sentences in the target language')
     parser.add_argument('--n', help='the number of examples per word')
-    parser.add_argument(
-        '--ignore-case', help='lowercase all words (recommended except for, e.g., German)', action=argparse.BooleanOptionalAction)
     parser.add_argument('--blocklist-filename',
                         help='a filename with vulgar or offensive words that should be excluded')
     parser.add_argument(
         '--min', help='the minimum number of occurrences of a word')
 
-    args = parser.parse_args()
-    raw_freqs = get_frequencies(
-        args.target_sentences_filename, args.language, args.ignore_case)
-    sorted_freqs = sorted(
-        raw_freqs.items(), key=lambda kvp: kvp[1], reverse=True)
-    freqs = {x[0]: index for index, x in enumerate(sorted_freqs)}
+    parser.add_argument(
+        '--base-ignore-case', help='lowercase all words (recommended except for, e.g., German)', action=argparse.BooleanOptionalAction)
+    parser.add_argument(
+        '--target-ignore-case', help='lowercase all words (recommended except for, e.g., German)', action=argparse.BooleanOptionalAction)
+    parser.add_argument(
+        '--get-base-examples', help='find simple target language sentences that contain base language words', action=argparse.BooleanOptionalAction)
 
-    word_set = {key: [] for key,
-                value in raw_freqs.items() if value >= int(args.min)}
+    args = parser.parse_args()
+    target_freqs = get_frequencies(
+        args.target_sentences_filename, args.target_language, args.target_ignore_case)
+    sorted_freqs = sorted(
+        target_freqs.items(), key=lambda kvp: kvp[1], reverse=True)
+    ranked_freqs = {x[0]: index for index, x in enumerate(sorted_freqs)}
+
+    word_set = {}
+    if args.get_base_examples:
+        # if attempting to build an index on base language, we still need target frequency
+        # because we're looking for the simplest target language sentences with base word.
+        # we do not need rankings of base words by frequency, however.
+        base_freqs = get_frequencies(
+            args.base_sentences_filename, args.base_language, args.base_ignore_case)
+        word_set = {key: [] for key,
+                    value in base_freqs.items() if value >= int(args.min)}
+    else:
+        word_set = {key: [] for key,
+                    value in target_freqs.items() if value >= int(args.min)}
 
     blocklist = get_blocklist(args.blocklist_filename)
 
@@ -104,7 +121,7 @@ def main():
                 target = line.strip()
                 base = base_file.readline().strip()
                 target_tokens = tokenize(
-                    target, args.language, args.ignore_case)
+                    target, args.target_language, args.target_ignore_case)
 
                 key = target.translate(str.maketrans(
                     '', '', string.punctuation)).replace(' ', '')
@@ -116,22 +133,34 @@ def main():
                     continue
 
                 target_words = get_words_with_punctuation(
-                    target, args.language)
+                    target, args.target_language)
+                base_words = get_words_with_punctuation(
+                    base, args.base_language)
                 average_freq = get_average_frequency_rank(
-                    freqs, target_tokens)
+                    ranked_freqs, target_tokens)
 
-                for word in target_tokens:
+                if args.get_base_examples:
+                    tokens = tokenize(base, args.base_language,
+                                      args.base_ignore_case)
+                else:
+                    tokens = target_tokens
+
+                for word in tokens:
                     if word not in word_set:
                         continue
                     if len(word_set[word]) < int(args.n):
                         heappush(word_set[word],
-                                 (-average_freq, target_words, base))
+                                 (-average_freq, target_words, base_words))
                     else:
                         heappushpop(
-                            word_set[word], (-average_freq, target_words, base))
+                            word_set[word], (-average_freq, target_words, base_words))
 
-    print(json.dumps({key: {'freq': freqs[key], 'examples': [(x[1], x[2]) for x in value]}
-                      for key, value in word_set.items()}, ensure_ascii=False))
+    if args.get_base_examples:
+        print(json.dumps({key: {'examples': [(x[1], x[2]) for x in sorted(value, key=lambda kvp:kvp[0], reverse=True)]}
+                          for key, value in word_set.items()}, ensure_ascii=False))
+    else:
+        print(json.dumps({key: {'freq': ranked_freqs[key], 'examples': [(x[1], x[2]) for x in sorted(value, key=lambda kvp:kvp[0], reverse=True)]}
+                          for key, value in word_set.items()}, ensure_ascii=False))
 
 
 if __name__ == '__main__':
